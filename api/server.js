498dd3d422414939726db2883b6a6750709a24a7
Noble Obioma <nobioma1@gmail.com>
2019-08-09 19:01:33 +0100

    Use app route on entry file

--- api/server.js
+++ api/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 
+const appRouter = require('./routes/appRouter');
+
 const server = express();
 
 server.use(cors());
 server.use(express.json());
+
+server.use('/api/v1', appRouter);
 
 server.get('/', (req, res) => {
   res.status(200).json({ message: 'Welcome to Pengin API' });
