77855b040dd2ea3980227ca0a79780258066d1c0
Noble Obioma <nobioma1@gmail.com>
2019-08-12 14:23:49 +0100

    Create migration and model for userSecretToken

---
+++ migrations/20190809200121-create-user-secret-token.js
@@ -0,0 +1,25 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('UserSecretTokens', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER,
+    },
+    userId: {
+      type: Sequelize.UUID,
+    },
+    token: {
+      type: Sequelize.STRING,
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+  }),
+  down: queryInterface => queryInterface.dropTable('UserSecretTokens'),
+};
---
+++ models/usersecrettoken.js
@@ -0,0 +1,17 @@
+module.exports = (sequelize, DataTypes) => {
+  const UserSecretToken = sequelize.define(
+    'UserSecretToken',
+    {
+      userId: DataTypes.UUID,
+      token: DataTypes.STRING,
+    },
+    {},
+  );
+  UserSecretToken.associate = (models) => {
+    const { User } = models;
+    UserSecretToken.belongsTo(User, {
+      foreignKey: 'userId',
+    });
+  };
+  return UserSecretToken;
+};
