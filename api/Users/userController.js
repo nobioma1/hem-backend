7b94168a22981330d193c88fe077aab483f1d4a3
Noble Obioma <nobioma1@gmail.com>
2019-08-12 14:18:32 +0100

    Create User Controllers:   - createUser: Creates a user, sends verification email, return token   - getUser: Returns a user profile

--- api/Users/userController.js
+++ api/Users/userController.js
@@ -1,14 +1,35 @@
-const { User } = require('../../models');
-const { generateToken } = require('../../helpers/authToken');
+const { User, UserSecretToken } = require('../../models');
+const {
+  authToken: { generateToken },
+  passwordHash: { compareHashPassword },
+  secretToken,
+  sendMail,
+} = require('../../helpers');
 
 const createUser = async (req, res) => {
   try {
-    const { id, firstname, email } = await User.create(req.body);
+    const { id, email, firstname } = await User.create({ ...req.body });
+    const confirmCode = secretToken();
+    await UserSecretToken.create({ userId: id, token: confirmCode });
+    await sendMail(email, confirmCode);
     const token = generateToken(id, email);
-    return res.status(201).json({ message: `Welcome, ${firstname}`, token });
+    return res.status(201).json({
+      message: `Welcome, ${firstname}, Please check email to verify account`,
+      token,
+    });
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
 };
 
-module.exports = { createUser };
+const getUser = async (req, res) => {
+  const { user } = req;
+  return res.status(200).json(user);
+};
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+  createUser,
+  getUser,
