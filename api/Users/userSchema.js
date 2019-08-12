f128f646aca79b222529cd6902ff26adfda23698
Noble Obioma <nobioma1@gmail.com>
2019-08-12 14:22:25 +0100

    Create schema to validate request body

--- api/Users/userSchema.js
+++ api/Users/userSchema.js
@@ -1,11 +1,24 @@
 const Joi = require('@hapi/joi');
 
-module.exports = Joi.object().keys({
-  firstname: Joi.string().required(),
-  lastname: Joi.string().required(),
-  email: Joi.string().required(),
-  password: Joi.string()
-    .min(5)
-    .required(),
-  profileURL: Joi.string().allow(''),
-});
+module.exports = {
+  register: Joi.object().keys({
+    firstname: Joi.string().required(),
+    lastname: Joi.string().required(),
+    email: Joi.string()
+      .required()
+      .email({ minDomainSegments: 2 }),
+    password: Joi.string()
+      .min(5)
+      .required(),
+    profileURL: Joi.string().allow(''),
+  }),
+  login: Joi.object().keys({
+    email: Joi.string()
+      .required()
+      .email({ minDomainSegments: 2 }),
+    password: Joi.string().required(),
+  }),
+  token: Joi.object().keys({
+    secretToken: Joi.string().required(),
+  }),
+};
