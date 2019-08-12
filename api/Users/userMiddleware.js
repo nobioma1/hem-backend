3a3827f0263fe10f861ab89c46b41e9844bf3f12
Noble Obioma <nobioma1@gmail.com>
2019-08-12 14:21:26 +0100

    Create middlewares:  - validateEmail: Check if the email is valid  - validateId: Check if user with id exist  - tokenExists: Check if verification token is valid  - checkStatus: Check the Status of a user if its active

--- api/Users/userMiddleware.js
+++ api/Users/userMiddleware.js
@@ -1,4 +1,4 @@
-const { User } = require('../../models');
+const { User, UserSecretToken } = require('../../models');
 
 module.exports = {
   userExists: async (req, res, next) => {
@@ -7,5 +7,42 @@ module.exports = {
       return next();
     }
     return res.status(400).json({ error: 'User with Email already exists' });
+  },
+  validateEmail: async (req, res, next) => {
+    const user = await User.findOne({ where: { email: req.body.email } });
+    if (user) {
+      req.user = user;
+      return next();
+    }
+    return res.status(404).json({ error: 'User with Email Does not exist' });
+  },
+  validateId: async (req, res, next) => {
+    const user = await User.findOne({ where: { id: req.params.id } });
+    if (user) {
+      delete user.dataValues.password;
+      req.user = user;
+      return next();
+    }
+    return res.status(404).json({ error: 'User Does not exist' });
+  },
+  tokenExists: async (req, res, next) => {
+    const { authUser, body } = req;
+    const secretToken = await UserSecretToken.findOne({
+      where: { token: body.secretToken, userId: authUser.id },
+    });
+    if (secretToken) {
+      return next();
+    }
+    return res.status(400).json({ error: 'Invalid Token' });
+  },
+
+  checkStatus: async (req, res, next) => {
+    const { id } = req.authUser;
+    const { isActive } = await User.findOne({ where: { id } });
+
+    if (!isActive) {
+      return next();
+    }
+    return res.status(400).json({ error: 'User Account is already active' });
   },
 };
