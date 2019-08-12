a67ad04632d21b06bbe27fc609c81eeb4ea56441
Noble Obioma <nobioma1@gmail.com>
2019-08-12 14:13:46 +0100

    Create Auth middlewares  - authUser: Checks for jwt token in header  - userActive: Checks if user account is actuve(verified)

---
+++ api/Auth/authMiddleware.js
@@ -0,0 +1,29 @@
+const { verifyToken } = require('../../helpers/authToken');
+const { User } = require('../../models');
+
+module.exports = {
+  authUser: async (req, res, next) => {
+    try {
+      const token = req.headers.authorization;
+      const decoded = verifyToken(token);
+      req.authUser = decoded;
+      return next();
+    } catch (error) {
+      return res.status(401).json({ error: error.message });
+    }
+  },
+  userActive: async (req, res, next) => {
+    const { id } = req.authUser;
+    try {
+      const { isActive } = await User.findOne({ where: { id } });      
+      if (!isActive) {
+        throw new Error();
+      }
+      return next();
+    } catch (error) {
+      return res
+        .status(401)
+        .json({ error: 'User Account is not active, Please Verify Account' });
+    }
+  },
+};
